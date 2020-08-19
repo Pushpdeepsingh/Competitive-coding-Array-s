8a5d60da7af9400a22246ed551ff09a6496b8d3f
pushpdeepsingh <56904281+Pushpdeepsingh@users.noreply.github.com>
2020-08-19 10:53:28 +0530

    Add files via upload

---
+++ beautiful triplets.cpp
@@ -0,0 +1,48 @@
+// beautiful triplets.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //input
+	cout << "enter lenght of the array" << endl;
+	int n; cin >> n;
+	cout << "enter the elements" << endl;
+	long int a[5];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	long int d;
+	cout << "enter the value of the d" << endl;
+	cin >> d;
+	//func
+	int count = 0;
+	for (int i = 0; i <= n - 3; i++)
+	{
+		for (int j = 1; j <= n - 2; j++)
+		{
+			for (int k = 2; i <= n- 1; k++)
+			{
+				if ((a[j] - a[i]) == (a[k] - a[j]) && (a[k] - a[j])==d)
+				{
+					count++;
+				}
+			}
+		}
+	}
+	cout << count << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
